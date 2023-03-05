1be7c596c9e2bc92e02fcc7956730b6322f6c31b
smoreyaks <bnixon.eng@gmail.com>
2023-03-06 00:54:48 +1100

    Headline Card Component w/ Styling

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero'
+import HeadlineCards from './components/HeadlineCards';
 
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <div className="App">
       <Navbar />
       <Hero />
+      <HeadlineCards />
     </div>
   );
 }
