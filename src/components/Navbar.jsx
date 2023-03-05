f130fa607b026bc67f527ea0e59f189545d362ca
smoreyaks <bnixon.eng@gmail.com>
2023-03-05 23:49:55 +1100

    Sidebar Overlay Function & State

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { AiOutlineMenu, AiOutlineSearch, AiOutlineClose, AiFillTag } from 'react-icons/ai';
 import { TbTruckDelivery } from 'react-icons/tb';
@@ -9,12 +9,15 @@ import { MdHelp, MdFavorite } from 'react-icons/md'
 
 
 const Navbar = () => {
+    // State
+    const [nav, setNav] = useState(false);
     
     return (
         <div className='max-w-[1640px] mx-auto flex justify-between items-center p-4'>
+            
             {/* Left Side */}
             <div className='flex items-center'>
-                <div className='cursor-pointer'>
+                <div onClick={()=> setNav(!nav)} className='cursor-pointer'>
                     <AiOutlineMenu size={30} />
                 </div>
                 <h1 className='text-2xl sm:text-3xl lg:text-4xl px-2'>
@@ -25,6 +28,7 @@ const Navbar = () => {
                     <p className='p-2'>Pickup</p>
                 </div>
             </div>
+
             {/* Search Input*/}
             <div className='bg-gray-200 rounded-full flex items-center px-2 w-[200px] sm:w-[400px] lg:w-[500px]'>
                 <AiOutlineSearch size={25} />
@@ -37,12 +41,18 @@ const Navbar = () => {
             <button className='bg-black text-white hidden md:flex items-center py-2 rounded-full'>
                 <BsFillCartFill size={20} className='mr-2' /> Cart
             </button>
+            
             {/* Mobile Menu */}
-            <div className='bg-black/80 fixed w-full h-screen z-10 top-0 left-0'></div>
+            {/* Overlay*/}
+            {nav ? <div className='bg-black/80 fixed w-full h-screen z-10 top-0 left-0'></div> : ''}
+            
             
             {/* Side Drawer */}
-            <div className='fixed top-0 left-0 w-[300px] h-screen bg-white z-10 duration-300'>
-                <AiOutlineClose size={30} className='absolute right-4 top-4 cursor-pointer' />
+            <div className={nav ? 'fixed top-0 left-0 w-[300px] h-screen bg-white z-10 duration-300' : 'fixed top-0 left-[-100%] w-[300px] h-screen bg-white z-10 duration-300'}>
+                <AiOutlineClose
+                    onClick={() => setNav(!nav)}
+                    size={30} 
+                    className='absolute right-4 top-4 cursor-pointer' />
                 <h2 className='text-2xl p-4'>Best <span className='font-bold'>Eats</span></h2>
                 <nav>
                     <ul className='flex flex-col p-4 text-gray-800'>
